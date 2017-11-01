a537c16286baf4c0ee6fc216b1fe4fbbaa58c307
Zaki Ahmed <zakiahmed@Zakis-MacBook-Pro.local>
2017-11-01 12:23:52 +0000

    Add Navigation Between Components

---
+++ src/app/app-routing.module.ts
@@ -0,0 +1,23 @@
+import { NgModule } from '@angular/core';
+import { Routes, RouterModule } from '@angular/router';
+import { HomeComponent } from './home/home.component';
+import { AboutComponent } from './about/about.component';
+import { ContactComponent } from './contact/contact.component';
+import { PropertiesComponent } from './properties/properties.component';
+
+
+const routes: Routes = [
+    { path: '', redirectTo: '/home', pathMatch: 'full' }, 
+    { path: 'home', component: HomeComponent },
+    { path: 'about', component: AboutComponent },
+    { path: 'contact', component: ContactComponent },
+    { path: 'properties', component: PropertiesComponent },
+    
+];
+
+@NgModule({
+    imports: [RouterModule.forRoot(routes)],
+    exports: [RouterModule]
+})
+
+export class AppRoutingModule { } 
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,13 +2,26 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { AboutComponent } from './about/about.component';
+import { ContactComponent } from './contact/contact.component';
+import { AppRoutingModule } from './app-routing.module';
+import { HomeComponent } from './home/home.component';
+import { PropertiesComponent } from './properties/properties.component';
+
+
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    AboutComponent,
+    ContactComponent,
+    HomeComponent,
+    PropertiesComponent,
+
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    AppRoutingModule
   ],
   providers: [],
   bootstrap: [AppComponent]
