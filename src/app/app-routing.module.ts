df55fd48e2d0a48a85bc326d8ba83a000ddc412c
Zaki Ahmed <zakiahmed@Zakis-MacBook-Pro.local>
2017-11-02 14:26:53 +0000

    Update Connection Part 12

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -16,7 +16,7 @@ const routes: Routes = [
 ];
 
 @NgModule({
-    imports: [RouterModule.forRoot(routes, { useHash: false })],
+    imports: [RouterModule.forRoot(routes)],
     exports: [RouterModule]
 })
 
